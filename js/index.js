d6a0418cbc0641ec1a84e75aaf0f55f6d380654f
lys <1031888705@qq.com>
2017-02-26 09:57:16 +0800

    blog

---
+++ js/index.js
@@ -0,0 +1,96 @@
+/**
+ * Created by lys on 2017/2/24.
+ */
+$(function () {
+    var fullPages=document.getElementById("fullpage");
+    var menu=document.getElementById("menu");
+    $("#menu>li").on("mouseover", function () {
+        $(this).addClass('animated tada');
+    })
+    $("#menu>li").on("mouseout", function () {
+        $(this).removeClass('animated tada');
+    })
+    var circleMenu=document.getElementById("circleMenu");
+    fullPage(fullPages,menu);
+    fullPage(fullPages,circleMenu);
+
+
+    window.onscroll= function () {
+        if(window.pageYOffset>menu.offsetTop){
+            menu.className="fixed";
+        }
+        if(window.pageYOffset<50){
+            menu.className="absoluted";
+        }
+    }
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        //水平还是垂直，vertical
+        direction: 'horizontal',
+//            direction: 'vertical',
+        loop: true,
+        //最初是第几页0表示第一页
+        initialSlide :0,
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+        //点击分页器的时候是否切换
+        paginationClickable :true,
+        //是否要自动播放，后面跟播放时间差
+        autoplay : 3000,
+        //用户操作过后是否停止滑动
+        autoplayDisableOnInteraction : false,
+        //滑动一下的时间
+        speed:300,
+        //鼠标放上去变成小手
+        grabCursor : true,
+        effect : 'coverflow',
+        slidesPerView: 2,
+        centeredSlides: true,
+        coverflow: {
+            rotate: 30,
+            stretch: 0,
+            depth: 100,
+            modifier: 2,
+            slideShadows : true
+        },
+        //是否支持键盘控制
+        keyboardControl : true,
+        //是否支持滚轮
+        mousewheelControl : true,
+        // 如果需要前进后退按钮
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+
+    })
+
+
+    circle(300,100,80,288);
+    circle(500,100,80,10);
+    circle(700,100,80,10);
+    circle(900,100,80,10);
+    circle(1100,100,80,10);
+    circle(1300,100,80,10);
+    function circle(x,y,r,reg){
+        var canvas=document.getElementById("cas");
+        var ctx=canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(x,y,r,0,360*Math.PI/180);
+        ctx.closePath();
+        ctx.fillStyle="hotpink";
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+        ctx.arc(x,y,r,-90*Math.PI/180,(reg-90)*Math.PI/180);
+        ctx.closePath();
+        ctx.fillStyle="white";
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.arc(x,y,r-10,0,360*Math.PI/180);
+        ctx.closePath();
+        ctx.fillStyle="#34353a";
+        ctx.fill();
+    }
+
+})
